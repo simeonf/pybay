0e85c7a56e856ea56932639b69baffa1b41b042f
Simeon Franklin <sfranklin@twitter.com>
2017-04-24 23:59:55 -0700

    Add separate prod and staging settings and wsgi files

---
+++ pybay/prod_settings.py
@@ -0,0 +1,27 @@
+import os
+from pybay.settings import *
+
+DEBUG = False
+TEMPLATE_DEBUG = DEBUG
+EMAIL_DEBUG = DEBUG
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.mysql",
+        'OPTIONS': {
+            'read_default_file': '/home/pybay/pybay_prod.cnf',
+        },
+    }
+}
+
+ALLOWED_HOSTS = ['prod.pyconfsf.com', 'pyconfsf.com', 'www.pyconfsf.com']
+
+TIME_ZONE = "US/Pacific"
+
+MEDIA_ROOT = '/data/websites/prod_site_media/media'
+MEDIA_URL = "/site_media/media/"
+
+STATIC_ROOT = '/data/websites/prod_site_media/static'
+STATIC_URL = "/site_media/static/"
+
+SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
