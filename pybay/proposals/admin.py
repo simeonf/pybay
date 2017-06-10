73545678b8025d74132554122069ab6cf5c8771a
Daniel Mizyrycki <mzdaniel@glidelink.net>
2017-06-09 17:43:33 -0700

    Add speaker in talk proposal admin list

--- pybay/proposals/admin.py
+++ pybay/proposals/admin.py
@@ -3,6 +3,7 @@ from django.contrib import admin
 from .models import TalkProposal, TutorialProposal
 
 class TalkProposalAdmin(admin.ModelAdmin):
+    list_display = ('title', 'speaker')
     pass
 
 admin.site.register(TalkProposal, TalkProposalAdmin)
