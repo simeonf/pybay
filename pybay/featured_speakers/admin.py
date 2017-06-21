36e6955594de2ce567eab05174e859cb52ffea65
Simeon Franklin <sfranklin@twitter.com>
2017-06-21 08:54:13 -0700

    Use name in admin dropdown list

--- pybay/featured_speakers/admin.py
+++ pybay/featured_speakers/admin.py
@@ -1,3 +1,4 @@
+from django.db import models
 from django.contrib import admin
 
 from ordered_model.admin import OrderedModelAdmin
@@ -6,5 +7,8 @@ from .models import FeaturedSpeaker
 
 class FeaturedSpeakerAdmin(OrderedModelAdmin):
     list_display = ('title', 'speaker', 'move_up_down_links')
+    formfield_overrides = {
+        models.OneToOneField: {'to_field_name': 'name'},
+    }
 
 admin.site.register(FeaturedSpeaker, FeaturedSpeakerAdmin)
