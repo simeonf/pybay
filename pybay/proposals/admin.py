c0073123ccd99a91bb1dd48319c40fca939109bb
Daniel Mizyrycki <mzdaniel@glidelink.net>
2017-07-01 20:03:44 -0700

    Add email and phone to admin talk listing

--- pybay/proposals/admin.py
+++ pybay/proposals/admin.py
@@ -4,7 +4,12 @@ from .models import TalkProposal, TutorialProposal
 
 
 class TalkProposalAdmin(admin.ModelAdmin):
-    list_display = ('title', 'speaker', 'status')
+    list_display = ('title', 'speaker', 'speaker_email', 'speaker_phone_number', 'status')
+    ordering = ['result__status', 'speaker']
+
+    def speaker_phone_number(self, obj):
+        return obj.speaker.phone_number
+    speaker_phone_number.admin_order_field  = 'speaker__phone_number'
 
 
 admin.site.register(TalkProposal, TalkProposalAdmin)
