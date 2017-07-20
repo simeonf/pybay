699e210dac8ed2809638d4bb1e3daa6105048a8b
Simeon Franklin <sfranklin@twitter.com>
2017-07-19 20:21:47 -0700

    Add custom admin for tutorials

--- pybay/proposals/admin.py
+++ pybay/proposals/admin.py
@@ -1,3 +1,4 @@
+from django import forms
 from django.contrib import admin
 
 from .models import TalkProposal, TutorialProposal
@@ -11,6 +12,18 @@ class TalkProposalAdmin(admin.ModelAdmin):
         return obj.speaker.phone_number
     speaker_phone_number.admin_order_field  = 'speaker__phone_number'
 
+class TutorialForm(forms.ModelForm):
+    """Custom form just to over-ride UX limit of 400 chars on description."""
+    description = forms.CharField(max_length=1200,
+                                  widget=forms.Textarea(attrs={'class': 'vLargeTextField'}))
+
+    class Meta:
+        model = TutorialProposal
+        exclude = ['name']
+
+
+class TutorialProposalAdmin(admin.ModelAdmin):
+    form = TutorialForm
 
 admin.site.register(TalkProposal, TalkProposalAdmin)
-admin.site.register(TutorialProposal)
+admin.site.register(TutorialProposal, TutorialProposalAdmin)
