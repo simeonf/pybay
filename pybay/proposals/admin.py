6c02a12a6f0fe03a01dc35a7dd2bedd05f9c250e
Simeon Franklin <sfranklin@twitter.com>
2018-05-08 14:08:20 -0700

    Add better categories display, more records per page, search, and listview audience level.

--- pybay/proposals/admin.py
+++ pybay/proposals/admin.py
@@ -3,11 +3,38 @@ from django.contrib import admin
 
 from .models import TalkProposal, TutorialProposal
 
+THEME_CHOICES = ["Fundamentals", "Data", "DevOps", "Speed", "Community", "Hardware"]
+
+class ThemeFilter(admin.SimpleListFilter):
+    """Provide sane list of themes from ad-hoc data.
+
+    We used multiple strings in a single "theme" field to capture this. Should have been FK to normalize data.
+
+    Since it's not... let's do some data parsing and munging here.
+    """
+    title = 'Theme'
+
+    # Parameter for the filter that will be used in the URL query.
+    parameter_name = 'themes'
+
+    def lookups(self, request, model_admin):
+        return zip(THEME_CHOICES, THEME_CHOICES)
+
+    def queryset(self, request, queryset):
+        theme = self.value()
+        if theme:
+          return queryset.filter(themes__icontains=theme)
+        else:
+          return queryset
+
 
 class TalkProposalAdmin(admin.ModelAdmin):
-    list_display = ('title', 'themes', 'speaker', 'speaker_email', 'phone', 'status', 'submitted')
+    list_display = ('title', 'themes', 'speaker', 'speaker_email', 'phone', 'status',
+                    'audience_level', 'submitted')
+    search_fields = ['title', 'speaker', 'description']
     ordering = ['-submitted', 'result__status', 'speaker']
-    list_filter = ('themes',)
+    list_filter = (ThemeFilter,)
+    list_per_page = 200
 
     def speaker_phone_number(self, obj):
         return obj.speaker.phone
